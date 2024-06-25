2ef1ddde23220b418a3d7724049fc6663034f55e
moonseonghui <msh9980@naver.com>
2024-06-25 17:07:24 +0900

    init: @로 절대 경로 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
-import GlobalStyle from "./styles/GlobalStyle.tsx";
 import { ThemeProvider } from "@emotion/react";
-import theme from "./styles/theme.ts";
+import theme from "@/styles/theme.ts";
+import GlobalStyle from "@/styles/GlobalStyle.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
