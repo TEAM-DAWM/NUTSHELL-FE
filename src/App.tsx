d299a182db80e3395fc54005abed1c921209ee26
moonseonghui <msh9980@naver.com>
2024-06-25 16:12:05 +0900

    init: globalstyle reset css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <h1>
           Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        </h1>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
