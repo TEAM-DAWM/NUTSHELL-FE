b2f21cbb5b9098c6905cd11b8db055a15aa5826b
Kjiw0n <rwd0904@naver.com>
2024-06-27 22:56:28 +0900

    chore: svgr 관련 설정 추가

--- src/assets/svg/IcCircleArrowRightSolid.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import type { SVGProps } from "react";
-const SvgIcCircleArrowRightSolid = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...props}>
-    <path d="M0 256a256 256 0 1 0 512 0 256 256 0 1 0-512 0m281 129c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l71-71L136 280c-13.3 0-24-10.7-24-24s10.7-24 24-24h182.1l-71-71c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L393 239c9.4 9.4 9.4 24.6 0 33.9z" />
-  </svg>
-);
-export default SvgIcCircleArrowRightSolid;
