75aa604497751d3a6e43249735510fce9972d063
moonseonghui <msh9980@naver.com>
2024-07-05 13:00:49 +0900

    fix: font theme 통합 적용

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,5 @@
+import { css } from '@emotion/react';
+
 const palette = {
 	// BLUE
 	BLUE_DISABLED: '#B9D0FF',
@@ -30,55 +32,112 @@ const palette = {
 };
 
 const fonts = {
-	DISPLAY_01: 'Pretendard-Bold',
-
-	TITLE_01: 'Pretendard-SemiBold',
-	TITLE_02: 'Pretendard-SemiBold',
-	TITLE_03: 'Pretendard-SemiBold',
-
-	HEADLINE_01: 'Pretendard-SemiBold',
-	HEADLINE_02: 'Pretendard-SemiBold',
-	HEADLINE_03: 'Pretendard-SemiBold',
-
-	BODY_01: 'Pretendard-Medium',
-	BODY_02: 'Pretendard-Medium',
-	BODY_03: 'Pretendard-Medium',
-	BODY_04: 'Pretendard-Medium',
-
-	LABEL_01: 'Pretendard-SemiBold',
-	LABEL_02: 'Pretendard-SemiBold',
-	LABEL_03: 'Pretendard-Medium',
-
-	CAPTION_01: 'Pretendard-Medium',
+	PretendardBold: 'Pretendard-Bold',
+	PretendardSemiBold: 'Pretendard-SemiBold',
+	PretendardMedium: 'Pretendard-Medium',
 };
 
-const fontSizes = {
-	DISPLAY_01: '64rem',
+const fontTheme = {
+	DISPLAY_01: css`
+		font-size: '64rem';
+		font-family: ${fonts.PretendardBold};
+		line-height: '76rem';
+		letter-spacing: '-2.5%';
+	`,
 
-	TITLE_01: '44rem',
-	TITLE_02: '32rem',
-	TITLE_03: '24rem',
+	TITLE_01: css`
+		font-size: '44rem';
+		font-family: ${fonts.PretendardSemiBold};
+		line-height: '45rem';
+		letter-spacing: '-1%';
+	`,
+	TITLE_02: css`
+		font-size: '32rem';
+		font-family: ${fonts.PretendardSemiBold};
+		line-height: '40rem';
+		letter-spacing: '0%';
+	`,
+	TITLE_03: css`
+		font-size: '24rem';
+		font-family: ${fonts.PretendardSemiBold};
+		line-height: '32rem';
+		letter-spacing: '0%';
+	`,
 
-	HEADLINE_01: '22rem',
-	HEADLINE_02: '20rem',
-	HEADLINE_03: '18rem',
+	HEADLINE_01: css`
+		font-size: '22rem';
+		font-family: ${fonts.PretendardSemiBold};
+		line-height: '30rem';
+		letter-spacing: '0%';
+	`,
+	HEADLINE_02: css`
+		font-size: '20rem';
+		font-family: ${fonts.PretendardSemiBold};
+		line-height: '28rem';
+		letter-spacing: '0%';
+	`,
+	HEADLINE_03: css`
+		font-size: '18rem';
+		font-family: ${fonts.PretendardSemiBold};
+		line-height: '24rem';
+		letter-spacing: '0%';
+	`,
 
-	BODY_01: '16rem',
-	BODY_02: '16rem',
-	BODY_03: '15rem',
-	BODY_04: '15rem',
+	BODY_01: css`
+		font-size: '16rem';
+		font-family: ${fonts.PretendardMedium};
+		line-height: '20rem';
+		letter-spacing: '-2%';
+	`,
+	BODY_02: css`
+		font-size: '16rem';
+		font-family: ${fonts.PretendardMedium};
+		line-height: '24rem';
+		letter-spacing: '-2%';
+	`,
+	BODY_03: css`
+		font-size: '15rem';
+		font-family: ${fonts.PretendardMedium};
+		line-height: '20rem';
+		letter-spacing: '-2%';
+	`,
+	BODY_04: css`
+		font-size: '15rem';
+		font-family: ${fonts.PretendardMedium};
+		line-height: '24rem';
+		letter-spacing: '-2%';
+	`,
 
-	LABEL_01: '14rem',
-	LABEL_02: '14rem',
-	LABEL_03: '14rem',
+	LABEL_01: css`
+		font-size: '14rem';
+		font-family: ${fonts.PretendardSemiBold};
+		line-height: '20rem';
+		letter-spacing: '-1%';
+	`,
+	LABEL_02: css`
+		font-size: '14rem';
+		font-family: ${fonts.PretendardSemiBold};
+		line-height: '24rem';
+		letter-spacing: '-1%';
+	`,
+	LABEL_03: css`
+		font-size: '14rem';
+		font-family: ${fonts.PretendardMedium};
+		line-height: '20rem';
+		letter-spacing: '-1%';
+	`,
 
-	CAPTION_01: '12rem',
+	CAPTION_01: css`
+		font-size: '12rem';
+		font-family: ${fonts.PretendardMedium};
+		line-height: '24rem';
+		letter-spacing: '-1%';
+	`,
 };
 
 export const theme = {
 	palette,
-	fonts,
-	fontSizes,
+	fontTheme,
 };
 
 export type ThemeType = typeof theme;
