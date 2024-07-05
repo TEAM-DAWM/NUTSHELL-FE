e497772a42076989d236b1d0a3a2c8f13f54cac1
wrryu09 <rryu09@ewhain.net>
2024-07-06 04:13:05 +0900

    style: arrow아이콘 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,7 +1,9 @@
+import Icn_arrow_narrow_right from '@/assets/svg/arrow-narrow-right.svg?react';
 import Icn_clock from '@/assets/svg/icn_clock.svg?react';
 
 const Icons = {
 	Icn_clock,
+	Icn_arrow_narrow_right,
 };
 
 export default Icons;
