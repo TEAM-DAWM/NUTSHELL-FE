4b7806967929b0c30762f33a2e054d63b5cc1678
moonseonghui <msh9980@naver.com>
2024-07-06 03:19:23 +0900

    init: storybook 초기세팅

---
+++ src/stories/Button/buttonStyle.ts
@@ -0,0 +1,44 @@
+import { css } from '@emotion/react';
+
+export const buttonStyles = css`
+	display: inline-block;
+
+	font-weight: 700;
+	font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+	line-height: 1;
+
+	cursor: pointer;
+	border: 0;
+	border-radius: 3px;
+
+	&.storybook-button-primary {
+		color: #fff;
+
+		background-color: #1ea7fd;
+	}
+
+	&.storybook-button-secondary {
+		color: #333;
+
+		background-color: transparent;
+		box-shadow: rgb(0 0 0 / 15%) 0 0 0 1px inset;
+	}
+
+	&.storybook-button-small {
+		padding: 10px 16px;
+
+		font-size: 1.2rem;
+	}
+
+	&.storybook-button-medium {
+		padding: 11px 20px;
+
+		font-size: 1.4rem;
+	}
+
+	&.storybook-button-large {
+		padding: 12px 24px;
+
+		font-size: 1.6rem;
+	}
+`;
