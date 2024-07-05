039b694325720ef30a09a6325ed711b909341323
wrryu09 <rryu09@ewhain.net>
2024-07-05 19:07:27 +0900

    feat: NotFound 페이지 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Calendar from './pages/Calendar';
 import DashBoard from './pages/DashBoard';
 import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 import Setting from './pages/Setting';
 import Today from './pages/Today';
 
@@ -15,6 +16,7 @@ const Router = () => {
 				<Route path="/dashboard" element={<DashBoard />} />
 				<Route path="/calendar" element={<Calendar />} />
 				<Route path="/setting" element={<Setting />} />
+				<Route path="/*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
 	);
