9479d60efc1bfa99500ea23049ca4a9d1819d43f
wrryu09 <rryu09@ewhain.net>
2024-07-06 22:17:34 +0900

    style: icon 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,9 +1,19 @@
 import Icn_arrow_narrow_right from '@/assets/svg/arrow-narrow-right.svg?react';
 import Icn_clock from '@/assets/svg/icn_clock.svg?react';
+import Icn_nav_calendar from '@/assets/svg/icn_nav_calendar.svg?react';
+import Icn_nav_dashboard from '@/assets/svg/icn_nav_dashboard.svg?react';
+import Icn_nav_setting from '@/assets/svg/icn_nav_setting.svg?react';
+import Icn_nav_today from '@/assets/svg/icn_nav_today.svg?react';
 
 const Icons = {
 	Icn_clock,
 	Icn_arrow_narrow_right,
+	Navbar: {
+		Icn_nav_calendar,
+		Icn_nav_dashboard,
+		Icn_nav_setting,
+		Icn_nav_today,
+	},
 };
 
 export default Icons;
