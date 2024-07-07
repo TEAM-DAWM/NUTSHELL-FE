6e7f8378ef382ff375916e6135c429882b2edb26
wrryu09 <rryu09@ewhain.net>
2024-07-07 23:02:59 +0900

    feat: RightArrangeBtn 초안

---
+++ src/components/common/arrangeBtn/RightArrangeBtn.tsx
@@ -0,0 +1,10 @@
+import Icons from "@/assets/svg/index"
+import { ArrangeBtnType } from "@/types/arrangeBtnType"
+
+function RightArrangeBtn({type, color}: ArrangeBtnType) {
+  return (
+    <Icons.ArrangeBtn.IcnArrangeRight/>
+  )
+}
+
+export default RightArrangeBtn
