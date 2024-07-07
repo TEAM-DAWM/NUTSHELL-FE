a1bf4a61cc7148e5b05df9c906ead9f834489b7c
wrryu09 <rryu09@ewhain.net>
2024-07-08 01:58:28 +0900

    style: add showcase

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -3,10 +3,69 @@ import ArrangeBtn from '@/components/common/arrangeBtn/ArrangeBtn';
 function DashBoard() {
 	return (
 		<div>
-			<ArrangeBtn type="right" color="blue" mode="default" size="small" />
-			<ArrangeBtn type="left" color="blue" mode="default" size="small" />
-			<ArrangeBtn type="set" color="blue" mode="default" size="small" />
-			<ArrangeBtn type="calendar" color="blue" mode="default" size="small" />
+			{/* right */}
+			<ArrangeBtn type="right" color="BLUE" mode="DEFAULT" size="small" />
+			<ArrangeBtn type="right" color="BLUE" mode="DISABLED" size="small" />
+			<ArrangeBtn type="right" color="BLUE" mode="HOVER" size="small" />
+			<ArrangeBtn type="right" color="BLUE" mode="PRESSED" size="small" />
+
+			<ArrangeBtn type="right" color="WHITE" mode="DEFAULT" size="small" />
+			<ArrangeBtn type="right" color="WHITE" mode="DISABLED" size="small" />
+			<ArrangeBtn type="right" color="WHITE" mode="HOVER" size="small" />
+			<ArrangeBtn type="right" color="WHITE" mode="PRESSED" size="small" />
+
+			<ArrangeBtn type="right" color="BLACK" mode="DEFAULT" size="small" />
+			<ArrangeBtn type="right" color="BLACK" mode="DISABLED" size="small" />
+			<ArrangeBtn type="right" color="BLACK" mode="HOVER" size="small" />
+			<ArrangeBtn type="right" color="BLACK" mode="PRESSED" size="small" />
+
+			{/* left */}
+			<ArrangeBtn type="left" color="BLUE" mode="DEFAULT" size="small" />
+			<ArrangeBtn type="left" color="BLUE" mode="DISABLED" size="small" />
+			<ArrangeBtn type="left" color="BLUE" mode="HOVER" size="small" />
+			<ArrangeBtn type="left" color="BLUE" mode="PRESSED" size="small" />
+
+			<ArrangeBtn type="left" color="WHITE" mode="DEFAULT" size="small" />
+			<ArrangeBtn type="left" color="WHITE" mode="DISABLED" size="small" />
+			<ArrangeBtn type="left" color="WHITE" mode="HOVER" size="small" />
+			<ArrangeBtn type="left" color="WHITE" mode="PRESSED" size="small" />
+
+			<ArrangeBtn type="left" color="BLACK" mode="DEFAULT" size="small" />
+			<ArrangeBtn type="left" color="BLACK" mode="DISABLED" size="small" />
+			<ArrangeBtn type="left" color="BLACK" mode="HOVER" size="small" />
+			<ArrangeBtn type="left" color="BLACK" mode="PRESSED" size="small" />
+
+			{/* set */}
+			<ArrangeBtn type="set" color="BLUE" mode="DEFAULT" size="small" />
+			<ArrangeBtn type="set" color="BLUE" mode="DISABLED" size="small" />
+			<ArrangeBtn type="set" color="BLUE" mode="HOVER" size="small" />
+			<ArrangeBtn type="set" color="BLUE" mode="PRESSED" size="small" />
+
+			<ArrangeBtn type="set" color="WHITE" mode="DEFAULT" size="small" />
+			<ArrangeBtn type="set" color="WHITE" mode="DISABLED" size="small" />
+			<ArrangeBtn type="set" color="WHITE" mode="HOVER" size="small" />
+			<ArrangeBtn type="set" color="WHITE" mode="PRESSED" size="small" />
+
+			<ArrangeBtn type="set" color="BLACK" mode="DEFAULT" size="small" />
+			<ArrangeBtn type="set" color="BLACK" mode="DISABLED" size="small" />
+			<ArrangeBtn type="set" color="BLACK" mode="HOVER" size="small" />
+			<ArrangeBtn type="set" color="BLACK" mode="PRESSED" size="small" />
+
+			{/* calendar */}
+			<ArrangeBtn type="calendar" color="BLUE" mode="DEFAULT" size="small" />
+			<ArrangeBtn type="calendar" color="BLUE" mode="DISABLED" size="small" />
+			<ArrangeBtn type="calendar" color="BLUE" mode="HOVER" size="small" />
+			<ArrangeBtn type="calendar" color="BLUE" mode="PRESSED" size="small" />
+
+			<ArrangeBtn type="calendar" color="WHITE" mode="DEFAULT" size="small" />
+			<ArrangeBtn type="calendar" color="WHITE" mode="DISABLED" size="small" />
+			<ArrangeBtn type="calendar" color="WHITE" mode="HOVER" size="small" />
+			<ArrangeBtn type="calendar" color="WHITE" mode="PRESSED" size="small" />
+
+			<ArrangeBtn type="calendar" color="BLACK" mode="DEFAULT" size="small" />
+			<ArrangeBtn type="calendar" color="BLACK" mode="DISABLED" size="small" />
+			<ArrangeBtn type="calendar" color="BLACK" mode="HOVER" size="small" />
+			<ArrangeBtn type="calendar" color="BLACK" mode="PRESSED" size="small" />
 		</div>
 	);
 }
