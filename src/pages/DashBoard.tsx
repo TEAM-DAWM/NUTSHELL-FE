e05f684c40b6f2e0b86088816a4dad73b79a07a6
wrryu09 <rryu09@ewhain.net>
2024-07-08 02:05:02 +0900

    style: 쇼케이스 수정

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,73 +1,61 @@
+import styled from '@emotion/styled';
+
 import ArrangeBtn from '@/components/common/arrangeBtn/ArrangeBtn';
 
 function DashBoard() {
 	return (
-		<div>
+		<>
 			{/* right */}
-			<ArrangeBtn type="right" color="BLUE" mode="DEFAULT" size="small" />
-			<ArrangeBtn type="right" color="BLUE" mode="DISABLED" size="small" />
-			<ArrangeBtn type="right" color="BLUE" mode="HOVER" size="small" />
-			<ArrangeBtn type="right" color="BLUE" mode="PRESSED" size="small" />
+			<Wrapper>
+				<ArrangeBtn type="right" color="BLUE" mode="DEFAULT" size="small" />
+				<ArrangeBtn type="right" color="BLUE" mode="DISABLED" size="small" />
 
-			<ArrangeBtn type="right" color="WHITE" mode="DEFAULT" size="small" />
-			<ArrangeBtn type="right" color="WHITE" mode="DISABLED" size="small" />
-			<ArrangeBtn type="right" color="WHITE" mode="HOVER" size="small" />
-			<ArrangeBtn type="right" color="WHITE" mode="PRESSED" size="small" />
+				<ArrangeBtn type="right" color="WHITE" mode="DEFAULT" size="small" />
+				<ArrangeBtn type="right" color="WHITE" mode="DISABLED" size="small" />
 
-			<ArrangeBtn type="right" color="BLACK" mode="DEFAULT" size="small" />
-			<ArrangeBtn type="right" color="BLACK" mode="DISABLED" size="small" />
-			<ArrangeBtn type="right" color="BLACK" mode="HOVER" size="small" />
-			<ArrangeBtn type="right" color="BLACK" mode="PRESSED" size="small" />
+				<ArrangeBtn type="right" color="BLACK" mode="DEFAULT" size="small" />
+				<ArrangeBtn type="right" color="BLACK" mode="DISABLED" size="small" />
+			</Wrapper>
 
 			{/* left */}
-			<ArrangeBtn type="left" color="BLUE" mode="DEFAULT" size="small" />
-			<ArrangeBtn type="left" color="BLUE" mode="DISABLED" size="small" />
-			<ArrangeBtn type="left" color="BLUE" mode="HOVER" size="small" />
-			<ArrangeBtn type="left" color="BLUE" mode="PRESSED" size="small" />
+			<Wrapper>
+				<ArrangeBtn type="left" color="BLUE" mode="DEFAULT" size="small" />
+				<ArrangeBtn type="left" color="BLUE" mode="DISABLED" size="small" />
 
-			<ArrangeBtn type="left" color="WHITE" mode="DEFAULT" size="small" />
-			<ArrangeBtn type="left" color="WHITE" mode="DISABLED" size="small" />
-			<ArrangeBtn type="left" color="WHITE" mode="HOVER" size="small" />
-			<ArrangeBtn type="left" color="WHITE" mode="PRESSED" size="small" />
+				<ArrangeBtn type="left" color="WHITE" mode="DEFAULT" size="small" />
+				<ArrangeBtn type="left" color="WHITE" mode="DISABLED" size="small" />
 
-			<ArrangeBtn type="left" color="BLACK" mode="DEFAULT" size="small" />
-			<ArrangeBtn type="left" color="BLACK" mode="DISABLED" size="small" />
-			<ArrangeBtn type="left" color="BLACK" mode="HOVER" size="small" />
-			<ArrangeBtn type="left" color="BLACK" mode="PRESSED" size="small" />
+				<ArrangeBtn type="left" color="BLACK" mode="DEFAULT" size="small" />
+				<ArrangeBtn type="left" color="BLACK" mode="DISABLED" size="small" />
+			</Wrapper>
 
 			{/* set */}
-			<ArrangeBtn type="set" color="BLUE" mode="DEFAULT" size="small" />
-			<ArrangeBtn type="set" color="BLUE" mode="DISABLED" size="small" />
-			<ArrangeBtn type="set" color="BLUE" mode="HOVER" size="small" />
-			<ArrangeBtn type="set" color="BLUE" mode="PRESSED" size="small" />
+			<Wrapper>
+				<ArrangeBtn type="set" color="BLUE" mode="DEFAULT" size="small" />
+				<ArrangeBtn type="set" color="BLUE" mode="DISABLED" size="small" />
 
-			<ArrangeBtn type="set" color="WHITE" mode="DEFAULT" size="small" />
-			<ArrangeBtn type="set" color="WHITE" mode="DISABLED" size="small" />
-			<ArrangeBtn type="set" color="WHITE" mode="HOVER" size="small" />
-			<ArrangeBtn type="set" color="WHITE" mode="PRESSED" size="small" />
+				<ArrangeBtn type="set" color="WHITE" mode="DEFAULT" size="small" />
+				<ArrangeBtn type="set" color="WHITE" mode="DISABLED" size="small" />
 
-			<ArrangeBtn type="set" color="BLACK" mode="DEFAULT" size="small" />
-			<ArrangeBtn type="set" color="BLACK" mode="DISABLED" size="small" />
-			<ArrangeBtn type="set" color="BLACK" mode="HOVER" size="small" />
-			<ArrangeBtn type="set" color="BLACK" mode="PRESSED" size="small" />
+				<ArrangeBtn type="set" color="BLACK" mode="DEFAULT" size="small" />
+				<ArrangeBtn type="set" color="BLACK" mode="DISABLED" size="small" />
+			</Wrapper>
 
 			{/* calendar */}
-			<ArrangeBtn type="calendar" color="BLUE" mode="DEFAULT" size="small" />
-			<ArrangeBtn type="calendar" color="BLUE" mode="DISABLED" size="small" />
-			<ArrangeBtn type="calendar" color="BLUE" mode="HOVER" size="small" />
-			<ArrangeBtn type="calendar" color="BLUE" mode="PRESSED" size="small" />
+			<Wrapper>
+				<ArrangeBtn type="calendar" color="BLUE" mode="DEFAULT" size="small" />
+				<ArrangeBtn type="calendar" color="BLUE" mode="DISABLED" size="small" />
 
-			<ArrangeBtn type="calendar" color="WHITE" mode="DEFAULT" size="small" />
-			<ArrangeBtn type="calendar" color="WHITE" mode="DISABLED" size="small" />
-			<ArrangeBtn type="calendar" color="WHITE" mode="HOVER" size="small" />
-			<ArrangeBtn type="calendar" color="WHITE" mode="PRESSED" size="small" />
+				<ArrangeBtn type="calendar" color="WHITE" mode="DEFAULT" size="small" />
+				<ArrangeBtn type="calendar" color="WHITE" mode="DISABLED" size="small" />
 
-			<ArrangeBtn type="calendar" color="BLACK" mode="DEFAULT" size="small" />
-			<ArrangeBtn type="calendar" color="BLACK" mode="DISABLED" size="small" />
-			<ArrangeBtn type="calendar" color="BLACK" mode="HOVER" size="small" />
-			<ArrangeBtn type="calendar" color="BLACK" mode="PRESSED" size="small" />
-		</div>
+				<ArrangeBtn type="calendar" color="BLACK" mode="DEFAULT" size="small" />
+				<ArrangeBtn type="calendar" color="BLACK" mode="DISABLED" size="small" />
+			</Wrapper>
+		</>
 	);
 }
-
+const Wrapper = styled.div`
+	display: flex;
+`;
 export default DashBoard;
