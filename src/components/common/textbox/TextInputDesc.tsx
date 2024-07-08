7c59f162ce04355fafd305f899d8642fce19f7fa
wrryu09 <rryu09@ewhain.net>
2024-07-08 15:57:41 +0900

    chore: 린트 적용

--- src/components/common/textbox/TextInputDesc.tsx
+++ src/components/common/textbox/TextInputDesc.tsx
@@ -7,10 +7,10 @@ function TextInputDesc({ type }: SizeType) {
 }
 
 const TextInputDescLayout = styled.textarea<{ type: string }>`
+	box-sizing: border-box;
 	width: ${({ type }) => (type === 'long' ? '34.8rem' : '30.4rem')};
 	height: ${({ type }) => (type === 'long' ? '14.5rem' : '18rem')};
 	padding: 1.2rem;
-	box-sizing: border-box;
 
 	${({ theme }) => theme.fontTheme.BODY_02};
 	border: solid 1px ${({ theme }) => theme.palette.GREY_01};
--- src/components/common/textbox/TextInputTime.tsx
+++ src/components/common/textbox/TextInputTime.tsx
@@ -19,10 +19,10 @@ function TextInputTime({ time }: TextInputTimeProps) {
 const InputTimeLayout = styled.div<{ time: string }>`
 	display: flex;
 	align-items: center;
+	box-sizing: border-box;
 	width: ${({ time }) => (time === 'total' ? '6rem' : '11.5rem')};
 	height: 3.2rem;
 	padding: 0.4rem 1rem;
-	box-sizing: border-box;
 
 	${({ theme }) => theme.fontTheme.BODY_02};
 	background-color: ${({ theme, time }) => (time !== 'total' ? theme.palette.GRAY_DISABLED : theme.palette.WITHE)};
@@ -38,9 +38,10 @@ const MinuteTxt = styled.p`
 `;
 const InputTimeStyle = styled.input<{ time: string }>`
 	${({ theme }) => theme.fontTheme.BODY_02};
+	box-sizing: border-box;
 	width: ${({ time }) => (time === 'total' ? '3.6rem' : '100%')};
 	padding: 0.4rem 0.8rem;
-	box-sizing: border-box;
+
 	background-color: ${({ theme, time }) => (time === 'total' ? theme.palette.GRAY_DISABLED : 'transparent')};
 	border: none;
 	border-radius: 8px;
