f7ee53ce7b7136b5fba3d79dcf05acb01e7bc1de
wrryu09 <rryu09@ewhain.net>
2024-07-08 16:12:32 +0900

    style: fontTheme 업데이트

--- src/styles/fontTheme.ts
+++ src/styles/fontTheme.ts
@@ -4,6 +4,7 @@ const fonts = {
 	PretendardBold: 'Pretendard-Bold',
 	PretendardSemiBold: 'Pretendard-SemiBold',
 	PretendardMedium: 'Pretendard-Medium',
+	PretendardRegular: 'Pretendard-Regular',
 };
 
 const fontTheme = {
@@ -11,14 +12,14 @@ const fontTheme = {
 		font-size: 6.4rem;
 		font-family: ${fonts.PretendardBold};
 		line-height: 7.6rem;
-		letter-spacing: -0.025em;
+		letter-spacing: -2.5%;
 	`,
 
 	TITLE_01: css`
 		font-size: 4.4rem;
 		font-family: ${fonts.PretendardSemiBold};
 		line-height: 4.5rem;
-		letter-spacing: -0.01em;
+		letter-spacing: -1%;
 	`,
 	TITLE_02: css`
 		font-size: 3.2rem;
@@ -55,58 +56,68 @@ const fontTheme = {
 	BODY_01: css`
 		font-size: 1.6rem;
 		font-family: ${fonts.PretendardMedium};
-		line-height: 2rem;
-		letter-spacing: -0.02em;
+		line-height: 2.1rem;
 	`,
 	BODY_02: css`
 		font-size: 1.6rem;
 		font-family: ${fonts.PretendardMedium};
 		line-height: 2.4rem;
-		letter-spacing: -0.02em;
+		letter-spacing: -2%;
 	`,
 	BODY_03: css`
 		font-size: 1.5rem;
 		font-family: ${fonts.PretendardMedium};
 		line-height: 2rem;
-		letter-spacing: -0.02em;
+		letter-spacing: -2%;
 	`,
 	BODY_04: css`
 		font-size: 1.5rem;
 		font-family: ${fonts.PretendardMedium};
 		line-height: 2.4rem;
-		letter-spacing: -0.02em;
+		letter-spacing: -2%;
 	`,
 
 	LABEL_01: css`
 		font-size: 1.4rem;
 		font-family: ${fonts.PretendardSemiBold};
 		line-height: 2rem;
-		letter-spacing: -0.01em;
+		letter-spacing: -1%;
 	`,
 	LABEL_02: css`
 		font-size: 1.4rem;
 		font-family: ${fonts.PretendardSemiBold};
 		line-height: 2.4rem;
-		letter-spacing: -0.01em;
+		letter-spacing: -1%;
 	`,
 	LABEL_03: css`
 		font-size: 1.4rem;
 		font-family: ${fonts.PretendardMedium};
 		line-height: 2rem;
-		letter-spacing: -0.01em;
+		letter-spacing: -1%;
 	`,
 
 	CAPTION_01: css`
 		font-size: 1.2rem;
-		font-family: ${fonts.PretendardMedium};
-		line-height: 2.4rem;
-		letter-spacing: -0.01em;
+		font-family: ${fonts.PretendardSemiBold};
+		line-height: 2rem;
+		letter-spacing: -1%;
 	`,
 	CAPTION_02: css`
 		font-size: 1.2rem;
 		font-family: ${fonts.PretendardMedium};
 		line-height: 2rem;
-		letter-spacing: -0.01em;
+		letter-spacing: -1%;
+	`,
+	CAPTION_03: css`
+		font-size: 1.2rem;
+		font-family: ${fonts.PretendardMedium};
+		line-height: 1.5rem;
+		letter-spacing: -1%;
+	`,
+	CAPTION_04: css`
+		font-size: 1.2rem;
+		font-family: ${fonts.PretendardRegular};
+		line-height: 2rem;
 	`,
 };
 export default fontTheme;
