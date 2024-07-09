19332ec00d718a1a2d102465c430f9428f608cdf
wrryu09 <rryu09@ewhain.net>
2024-07-09 18:26:13 +0900

    chore: datepicker 표시

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,63 +1,6 @@
-import styled from '@emotion/styled';
-
-import ArrangeBtn from '@/components/common/arrangeBtn/ArrangeBtn';
 import DatePickerCustom from '@/components/common/datePicker/DatePickerCustom';
 
 function DashBoard() {
-	return (
-		<>
-			{/* right */}
-			<Wrapper>
-				<ArrangeBtn type="right" color="BLUE" mode="DEFAULT" size="small" />
-				<ArrangeBtn type="right" color="BLUE" mode="DISABLED" size="small" />
-
-				<ArrangeBtn type="right" color="WHITE" mode="DEFAULT" size="small" />
-				<ArrangeBtn type="right" color="WHITE" mode="DISABLED" size="small" />
-
-				<ArrangeBtn type="right" color="BLACK" mode="DEFAULT" size="small" />
-				<ArrangeBtn type="right" color="BLACK" mode="DISABLED" size="small" />
-			</Wrapper>
-
-			{/* left */}
-			<Wrapper>
-				<ArrangeBtn type="left" color="BLUE" mode="DEFAULT" size="small" />
-				<ArrangeBtn type="left" color="BLUE" mode="DISABLED" size="small" />
-
-				<ArrangeBtn type="left" color="WHITE" mode="DEFAULT" size="small" />
-				<ArrangeBtn type="left" color="WHITE" mode="DISABLED" size="small" />
-
-				<ArrangeBtn type="left" color="BLACK" mode="DEFAULT" size="small" />
-				<ArrangeBtn type="left" color="BLACK" mode="DISABLED" size="small" />
-			</Wrapper>
-
-			{/* set */}
-			<Wrapper>
-				<ArrangeBtn type="set" color="BLUE" mode="DEFAULT" size="small" />
-				<ArrangeBtn type="set" color="BLUE" mode="DISABLED" size="small" />
-
-				<ArrangeBtn type="set" color="WHITE" mode="DEFAULT" size="small" />
-				<ArrangeBtn type="set" color="WHITE" mode="DISABLED" size="small" />
-
-				<ArrangeBtn type="set" color="BLACK" mode="DEFAULT" size="small" />
-				<ArrangeBtn type="set" color="BLACK" mode="DISABLED" size="small" />
-			</Wrapper>
-
-			{/* calendar */}
-			<Wrapper>
-				<ArrangeBtn type="calendar" color="BLUE" mode="DEFAULT" size="small" />
-				<ArrangeBtn type="calendar" color="BLUE" mode="DISABLED" size="small" />
-
-				<ArrangeBtn type="calendar" color="WHITE" mode="DEFAULT" size="small" />
-				<ArrangeBtn type="calendar" color="WHITE" mode="DISABLED" size="small" />
-
-				<ArrangeBtn type="calendar" color="BLACK" mode="DEFAULT" size="small" />
-				<ArrangeBtn type="calendar" color="BLACK" mode="DISABLED" size="small" />
-			</Wrapper>
-			<DatePickerCustom />
-		</>
-	);
+	return <DatePickerCustom />;
 }
-const Wrapper = styled.div`
-	display: flex;
-`;
 export default DashBoard;
