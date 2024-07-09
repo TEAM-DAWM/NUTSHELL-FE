b2d18d7cab754c89e73a29064611a362e86a044d
JeeminYi <yjjmmm000@g.skku.edu>
2024-07-09 22:42:56 +0900

    feat: 대시보드 task 컴포넌트 구현

---
+++ src/components/DashboardPage/DashboardTask.tsx
@@ -0,0 +1,81 @@
+import styled from '@emotion/styled';
+
+interface DashboardTaskProps {
+	text: string;
+}
+
+function DashboardTask({ text }: DashboardTaskProps) {
+	return (
+		<TaskLayout>
+			<TextBox>
+				<TaskText text={text}>{text}</TaskText>
+				<NumberTextBox>
+					<Number>2</Number>
+					<NumberText>개</NumberText>
+				</NumberTextBox>
+			</TextBox>
+			<ScrollArea />
+		</TaskLayout>
+	);
+}
+export default DashboardTask;
+
+const TaskLayout = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	align-self: stretch;
+	width: 41.7rem;
+	height: 41rem;
+
+	border: 1px solid #dedede;
+	border-radius: 12px;
+`;
+
+const TextBox = styled.span`
+	display: flex;
+	justify-content: space-between;
+	width: 41.7rem;
+	height: 7rem;
+`;
+
+const TaskText = styled.div<{ text: string }>`
+	display: flex;
+	align-items: flex-start;
+	min-width: 11.8rem;
+	padding: 1.8rem 0 2rem 2.2rem;
+
+	${({ theme }) => theme.fontTheme.HEADLINE_02};
+	color: ${({ theme }) => theme.palette.Grey.Black};
+`;
+
+const NumberTextBox = styled.div`
+	display: flex;
+	align-items: center;
+	width: 3.8rem;
+	height: 4rem;
+	margin: 1.5rem 2.2rem 1.5rem 0;
+`;
+
+const Number = styled.p`
+	${({ theme }) => theme.fontTheme.TITLE_02};
+	color: ${({ theme }) => theme.palette.Grey.Grey8};
+`;
+
+const NumberText = styled.p`
+	${({ theme }) => theme.fontTheme.HEADLINE_02}
+	color: ${({ theme }) => theme.palette.Grey.Grey7}
+`;
+
+const ScrollArea = styled.div`
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	gap: 0.8rem;
+	align-items: stretch;
+	box-sizing: border-box;
+	width: 41.7rem;
+	height: 33.9rem;
+	padding: 0 0.7rem;
+	overflow: scroll;
+`;
