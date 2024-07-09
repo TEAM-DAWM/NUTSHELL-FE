8ce2dffed724e098c44cb9436c750c971f2d1184
wrryu09 <rryu09@ewhain.net>
2024-07-09 14:33:34 +0900

    style: 스타일링 진행

--- src/components/common/datePicker/DatePickerCustom.tsx
+++ src/components/common/datePicker/DatePickerCustom.tsx
@@ -1,11 +1,10 @@
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import ko from 'date-fns/locale/ko';
 import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
-import TextBtn from '../button/TextBtn';
+import TextBtn from '../button/textBtn/TextBtn';
 
 function DatePickerCustom() {
 	const [startDate, setStartDate] = useState(new Date());
@@ -26,15 +25,10 @@ function DatePickerCustom() {
 			inline
 			calendarContainer={Calendar}
 		>
-			<TextBtn text="닫기" color="black" size="small" isLight={false} />
+			<TextBtn text="닫기" color="BLACK" size="small" mode="DEFAULT" isHover isPressed />
 		</CustomPicker>
 	);
 }
-const spreadStyle = css`
-	display: flex;
-	justify-content: space-between;
-	width: 20.4rem;
-`;
 
 const CustomPicker = styled(({ className, ...props }) => <DatePicker {...props} wrapperClassName={className} />)`
 	width: 100%;
@@ -45,15 +39,26 @@ const Calendar = styled.div`
 	flex-direction: column;
 	align-items: center;
 	width: 22.8rem;
+	padding: 1.6rem 1.2rem;
 	overflow: hidden;
 
 	box-shadow: 0 3px 7px 0 rgb(0 0 0 / 38%);
 	border-radius: 12px;
-
+	/* stylelint-disable selector-class-pattern */
 	.react-datepicker__month-container {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+	}
+
+	.react-datepicker__month {
+		display: flex;
+		flex-wrap: wrap;
+		place-content: center center;
+		gap: 0.6rem;
+		margin: 0;
+		margin-bottom: 1.9rem;
+		${({ theme }) => theme.fontTheme.CAPTION_02};
 	}
 
 	/* 현재 달 안 보이게 */
@@ -63,7 +68,9 @@ const Calendar = styled.div`
 
 	/* 화살표 */
 	.react-datepicker__navigation {
-		/* display: block; */
+		position: static;
+
+		background-color: aliceblue;
 	}
 
 	.react-datepicker__header {
@@ -75,28 +82,20 @@ const Calendar = styled.div`
 
 	/* 요일 글자 */
 	.react-datepicker__day-names {
-		${spreadStyle}
 	}
 
 	.react-datepicker__day-name {
+		width: 2.8rem;
+		margin: 0;
+
 		color: ${({ theme }) => theme.palette.Grey.Grey5};
 		${({ theme }) => theme.fontTheme.CAPTION_04};
 	}
 
-	/* 달 영역 */
-	.react-datepicker__month {
-		${({ theme }) => theme.fontTheme.CAPTION_02};
-		display: flex;
-		place-content: center center;
-		flex-wrap: wrap;
-		gap: 0.7rem;
-		align-items: center;
-		align-self: stretch;
-	}
-
 	/** 주 날짜 */
 	.react-datepicker__week {
-		${spreadStyle}
+		display: flex;
+		width: 19.6rem;
 	}
 
 	/* 선택된 날짜 */
@@ -104,13 +103,16 @@ const Calendar = styled.div`
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		width: 2.2rem;
-		height: 2.2rem;
+		width: 100%;
+		height: 2.9rem;
 		margin: 0;
+
+		border-radius: 0;
 	}
 
 	.react-datepicker__day--selected {
 		background-color: ${({ theme }) => theme.palette.Primary};
+		border-radius: 6px;
 	}
 
 	.react-datepicker__day--in-selecting-range {
@@ -131,12 +133,21 @@ const Calendar = styled.div`
 		color: ${({ theme }) => theme.palette.Grey.White};
 
 		background-color: ${({ theme }) => theme.palette.Primary};
+		border-radius: 6px;
 	}
 
 	/* 이번 월에 포함되지 않는 날짜 */
 	.react-datepicker__day--outside-month {
 		color: ${({ theme }) => theme.palette.Grey.Grey4};
 	}
-`;
 
+	.react-datepicker__children-container {
+		align-self: flex-end;
+		width: fit-content;
+		margin: 0;
+		padding: 0;
+	}
+
+	/* stylelint-enable selector-class-pattern */
+`;
 export default DatePickerCustom;
