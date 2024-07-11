49bd8e1e0b21dc4aa768a2d4a6b37797b0158811
JeeminYi <yjjmmm000@g.skku.edu>
2024-07-11 22:49:50 +0900

    fix: text input 삭제

--- src/components/DashboardPage/DateArea.tsx
+++ src/components/DashboardPage/DateArea.tsx
@@ -1,25 +1,15 @@
 import styled from '@emotion/styled';
-import { useRef, useState } from 'react';
-
-import PeriodTextInput from './PeriodTextInput';
 
 import Icons from '@/assets/svg/index';
 
 function DateArea() {
-	const [date, setDate] = useState<Date | null>(new Date());
-	const inputRef = useRef<HTMLInputElement>(null);
-
-	// eslint-disable-next-line @typescript-eslint/no-shadow
-	const handleChange = (date: Date) => {
-		setDate(date);
-	};
 	return (
 		<DateAreaLayout>
 			<PlaceholderWrapper>
 				<StlyedCalendarIcon />
-				<PeriodTextInput startDate="2024-01-01" dateValue={date} onChange={handleChange} dateTextRef={inputRef} />
+				<HardCoding />
 				<StyledArrowIcon />
-				<PeriodTextInput endDate="2024-12-31" dateValue={date} onChange={handleChange} dateTextRef={inputRef} />
+				<HardCoding />
 			</PlaceholderWrapper>
 			<PastDateWrapper>
 				<PastWeekBtn>지난 1주일</PastWeekBtn>
@@ -61,15 +51,15 @@ const StlyedCalendarIcon = styled(Icons.Icn_calander)`
 	}
 `;
 
-// const HardCoding = styled.span`
-// 	display: flex;
-// 	align-items: center;
-// 	justify-content: center;
-// 	width: 13.9rem;
-// 	height: 3.2rem;
-// 	padding: 0.7rem 1.6rem;
-// 	${({ theme }) => theme.fontTheme.BODY_02};
-// `;
+const HardCoding = styled.span`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 13.9rem;
+	height: 3.2rem;
+	padding: 0.7rem 1.6rem;
+	${({ theme }) => theme.fontTheme.BODY_02};
+`;
 
 const StyledArrowIcon = styled(Icons.Icn_arrow_narrow_right)`
 	width: 2.4rem;
