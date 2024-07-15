f8a5bc730d4dcefa9cd18ba4255c688f77688cfb
moonseonghui <msh9980@naver.com>
2024-07-15 22:28:26 +0900

    feat: react-oauth/google설치 및 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from '@emotion/react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
@@ -10,15 +11,20 @@ import { theme } from '@/styles/theme.ts';
 
 import '@/styles/font.css';
 
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_LOGIN_CLIENT_ID;
+
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<GlobalStyle />
-			<ThemeProvider theme={theme}>
-				<App />
-			</ThemeProvider>
-			<ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" position="left" />
-		</QueryClientProvider>
-	</React.StrictMode>
+	<GoogleOAuthProvider clientId={CLIENT_ID}>
+		
+		<React.StrictMode>
+			<QueryClientProvider client={queryClient}>
+				<GlobalStyle />
+				<ThemeProvider theme={theme}>
+					<App />
+				</ThemeProvider>
+				<ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" position="left" />
+			</QueryClientProvider>
+		</React.StrictMode>
+	</GoogleOAuthProvider>
 );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,13 @@
+import { GoogleLogin } from '@react-oauth/google';
+
 function Login() {
-	return <>login</>;
+	const responseMessage = (response) => {
+		console.log(response);
+	};
+	const errorMessage = (error) => {
+		console.log(error);
+	};
+	return <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />;
 }
 
 export default Login;
