ac6db2722a7a97dd90c2d5c4b10e2cf9b295bfa0
JeeminYi <yjjmmm000@g.skku.edu>
2024-07-16 05:33:07 +0900

    fix: 시작 날짜/마감 날짜 수정

--- src/components/DashboardPage/DateArea.tsx
+++ src/components/DashboardPage/DateArea.tsx
@@ -11,10 +11,14 @@ interface DateAreaProps {
 	isPressed: boolean;
 }
 
+// startDate : 시작 날짜 ; endDate : 마감 날짜
+
 function DateArea({ isHover, isPressed }: DateAreaProps) {
 	const today = new Date();
-	const [startDate, setStartDate] = useState(today);
+	const [startDate, setStartDate] = useState<Date | null>(null);
 	const [endDate, setEndDate] = useState<Date | null>(null);
+	// const [startDate, setStartDate] = useState(today);
+	// const [endDate, setEndDate] = useState<Date | null>(null);
 	const [isClicked, setIsClicked] = useState(false);
 
 	const handleClick = () => {
@@ -22,34 +26,57 @@ function DateArea({ isHover, isPressed }: DateAreaProps) {
 	};
 
 	const handleStartDate = (date: Date | null) => {
-		setStartDate(date || today);
+		setStartDate(date);
 	};
 
 	const handleEndDate = (date: Date | null) => {
 		setEndDate(date);
 	};
 
+	// const handleStartDate = (date: Date | null) => {
+	// 	setStartDate(date || today);
+	// };
+
+	// const handleEndDate = (date: Date | null) => {
+	// 	setEndDate(date);
+	// };
+
 	const handleClickPastDate = (getPastDate: number) => {
-		const PastEndDate = new Date(startDate);
-		PastEndDate.setDate(startDate.getDate() - getPastDate);
-		handleEndDate(PastEndDate);
+		const newStartDate = new Date(today);
+		newStartDate.setDate(today.getDate() - getPastDate);
+		handleStartDate(newStartDate);
+
+		const newEndDate = new Date(today);
+		newEndDate.setDate(today.getDate());
+		handleEndDate(newEndDate);
 	};
+
+	// const handleClickPastDate = (getPastDate: number) => {
+	// 	const PastEndDate = new Date(startDate);
+	// 	PastEndDate.setDate(startDate.getDate() - getPastDate);
+	// 	handleEndDate(PastEndDate);
+	// };
 
 	const getPastDateWeek = 6;
 	const getPastDateMonth = 30;
-
 	useEffect(() => {
-		const newEndDate = new Date(startDate);
-		newEndDate.setDate(startDate.getDate() - 13);
-		handleEndDate(newEndDate);
-		handleStartDate(today);
-	}, []);
+		// 초기에는 endDate 기준으로 startDate 설정
+		const initialStartDate = new Date(today);
+		initialStartDate.setDate(today.getDate() - 14); // 2주 전
+		handleStartDate(initialStartDate);
+	}, []); // endDate가 변경될 때마다 초기값 설정
 
 	return (
 		<DatePickerCustomLayout>
 			<DateAreaLayout>
 				<DatePickerContainer>
-					<DatePickerPlaceholder isHover isPressed endDate={endDate} startDate={startDate} handleClick={handleClick} />
+					<DatePickerPlaceholder
+						isHover
+						isPressed
+						endDate={endDate || today}
+						startDate={startDate || today}
+						handleClick={handleClick}
+					/>
 					<DatePickerWrapper>
 						<DatePickerCustom
 							isOpen={isClicked}
