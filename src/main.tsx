a04291c09e9ed88728a8654d2a845bb708e523d8
Kjiw0n <rwd0904@naver.com>
2024-07-16 01:55:21 +0900

    chore: react-beautifil-dnd가 React 18의 StrictMode를 지원하지 않기에 해당 부분 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from '@emotion/react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from '@/App.tsx';
@@ -16,14 +15,12 @@ const CLIENT_ID = import.meta.env.VITE_GOOGLE_LOGIN_CLIENT_ID;
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<GoogleOAuthProvider clientId={CLIENT_ID}>
-		<React.StrictMode>
-			<QueryClientProvider client={queryClient}>
-				<GlobalStyle />
-				<ThemeProvider theme={theme}>
-					<App />
-				</ThemeProvider>
-				<ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" position="left" />
-			</QueryClientProvider>
-		</React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<GlobalStyle />
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+			<ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" position="left" />
+		</QueryClientProvider>
 	</GoogleOAuthProvider>
 );
