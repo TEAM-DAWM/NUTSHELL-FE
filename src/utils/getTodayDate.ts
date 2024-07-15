a89b037ae1c06cc2a60ae232eec410820648c2e3
JeeminYi <yjjmmm000@g.skku.edu>
2024-07-16 01:55:46 +0900

    chore: 중복 파일 삭제

--- src/utils/getTodayDate.ts
+++
@@ -1,8 +0,0 @@
-function getTodayDate() {
-	const today = new Date();
-	const date = today.getDate();
-	const month = today.getMonth() + 1;
-	const year = today.getFullYear();
-	return `${year}년 ${month}월 ${date}일`;
-}
-export default getTodayDate;
