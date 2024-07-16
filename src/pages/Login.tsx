669a16732aaf62ced17af3042b9cf65e79e49ed9
moonseonghui <msh9980@naver.com>
2024-07-16 20:15:17 +0900

    feat: 로그인 퍼블리싱 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,7 @@ import { useGoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
 
 import userLogin from '@/apis/login/loginAxios';
+import Images from '@/assets/images';
 
 function Login() {
 	const navigate = useNavigate();
@@ -27,11 +28,91 @@ function Login() {
 		scope: 'email profile',
 	});
 
-	return <GoogleBtn onClick={googleSocialLogin}>Google Button</GoogleBtn>;
+	return (
+		<LoginLayout>
+			<LeftSection>
+				<LogoTitleImg src={Images.titleIcon} />
+				<LoginBtn>
+					<GoogleBtn onClick={googleSocialLogin}>
+						<GoogleImg src={Images.googleIcon} />
+						구글 계정으로 시작하기
+					</GoogleBtn>
+					<SignDescription>
+						가입하면 자동으로 <a href="https://www.naver.com/">개인정보보호정책</a>과{' '}
+						<a href="https://www.naver.com/">이용약관</a>에 동의한 것으로 간주됩니다.
+					</SignDescription>
+				</LoginBtn>
+			</LeftSection>
+			<LoginImg src={Images.BG.loginBg} />
+		</LoginLayout>
+	);
 }
 
-const GoogleBtn = styled.button`
-	width: 30rem;
+const LoginLayout = styled.div`
+	display: flex;
+	justify-content: space-between;
+	width: 100vw;
+	height: 100vh;
 `;
 
+const LeftSection = styled.section`
+	display: flex;
+	flex-direction: column;
+	gap: 8.2rem;
+	align-items: center;
+	justify-content: center;
+	width: 50%;
+	height: 100%;
+`;
+
+const LoginImg = styled.img`
+	width: 50%;
+	height: 100%;
+`;
+
+const LogoTitleImg = styled.img`
+	width: 34rem;
+	height: 8rem;
+`;
+
+const LoginBtn = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 1.2rem;
+	align-items: center;
+	width: 32.4rem;
+	height: 9.5rem;
+`;
+
+const GoogleBtn = styled.button`
+	display: flex;
+	gap: 0.9rem;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	height: 4.3rem;
+
+	color: ${({ theme }) => theme.palette.Grey.White};
+
+	background-color: ${({ theme }) => theme.palette.Grey.Black};
+	border-radius: 12px;
+	${({ theme }) => theme.fontTheme.LABEL_03};
+`;
+
+const GoogleImg = styled.img`
+	width: 1.8rem;
+	height: 1.8rem;
+`;
+
+const SignDescription = styled.p`
+	width: 24rem;
+
+	color: ${({ theme }) => theme.palette.Grey.Grey5};
+	text-align: center;
+
+	${({ theme }) => theme.fontTheme.CAPTION_02};
+	a {
+		color: ${({ theme }) => theme.palette.Grey.Grey5};
+	}
+`;
 export default Login;
