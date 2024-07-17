2032d6775570e9f2842b68dd34bd06c2173d5b15
wrryu09 <rryu09@ewhain.net>
2024-07-17 16:23:50 +0900

    feat: PrivateRoute 설정

---
+++ src/router/Router.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Calendar from '../pages/Calendar';
+import DashBoard from '../pages/DashBoard';
+import Login from '../pages/Login';
+import MainLayout from '../pages/MainLayout';
+import NotFound from '../pages/NotFound';
+import Setting from '../pages/Setting';
+import Today from '../pages/Today';
+
+import PrivateRoute from './PrivateRoute';
+
+function Router() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route element={<PrivateRoute />}>
+					<Route element={<MainLayout />}>
+						<Route path="/today" element={<Today />} />
+						<Route path="/dashboard" element={<DashBoard />} />
+						<Route path="/calendar" element={<Calendar />} />
+						<Route path="/setting" element={<Setting />} />
+					</Route>
+				</Route>
+				<Route path="/*" element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default Router;
