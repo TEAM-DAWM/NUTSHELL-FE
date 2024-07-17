918f31d3038fddfbccdc7b5e18376666d18cd0bb
JeeminYi <yjjmmm000@g.skku.edu>
2024-07-18 02:06:30 +0900

    fix: image 태그 삽입

--- src/components/DashboardPage/TaskSummary.tsx
+++ src/components/DashboardPage/TaskSummary.tsx
@@ -10,45 +10,51 @@ interface TaskSummaryProps {
 
 function TaskSummary({ text, data, unit, icon: Icon, image }: TaskSummaryProps) {
 	return (
-		<TaskSummaryLayout image={image}>
-			<ProfileImg>
-				<Icon />
-			</ProfileImg>
-			<TextWrapper>
-				<SummaryText>{text}</SummaryText>
-				<NumberTaskBox>
-					<Number>{data}</Number>
-					<NumberText>{unit}</NumberText>
-				</NumberTaskBox>
-			</TextWrapper>
+		<TaskSummaryLayout>
+			<TextArea>
+				<ProfileImg>
+					<Icon />
+				</ProfileImg>
+				<TextWrapper>
+					<SummaryText>{text}</SummaryText>
+					<NumberTaskBox>
+						<Number>{data}</Number>
+						<NumberText>{unit}</NumberText>
+					</NumberTaskBox>
+				</TextWrapper>
+			</TextArea>
+			<ImageArea>
+				<ImageComponent src={image} />
+			</ImageArea>
 		</TaskSummaryLayout>
 	);
 }
 
 export default TaskSummary;
 
-const TaskSummaryLayout = styled.div<{ image: string }>`
+const TaskSummaryLayout = styled.div`
+	display: flex;
+	width: 41.7rem;
+	height: 18.2rem;
+
+	background-color: ${({ theme }) => theme.palette.Blue.Blue5};
+	border-radius: 16px;
+`;
+
+const TextArea = styled.div`
 	display: flex;
 	flex-direction: column;
 	gap: 2rem;
 	align-items: flex-start;
-	align-self: stretch;
-	justify-content: center;
-	box-sizing: border-box;
-	width: 41.7rem;
-	height: 18.2rem;
-	padding-left: 2.8rem;
-
-	background-color: ${({ theme }) => theme.palette.Blue.Blue5};
-	background-image: url(${(props) => props.image});
-	background-size: cover;
-	border-radius: 16px;
+	align-self: center;
+	width: 14.5rem;
+	height: 11.2rem;
+	margin-left: 2.8rem;
 `;
 
 const ProfileImg = styled.div`
 	width: 2.8rem;
 	height: 2.8rem;
-	margin: 3rem 34.5rem 0 0;
 
 	svg {
 		width: 100%;
@@ -60,15 +66,14 @@ const TextWrapper = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: flex-start;
-	align-self: stretch;
-	width: 38.9rem;
+	justify-content: flex-start;
+	width: 100%;
 	height: 6.4rem;
-	margin-bottom: 3.5rem;
 `;
 
 const SummaryText = styled.p`
-	${({ theme }) => theme.fontTheme.HEADLINE_03}
-	color: ${({ theme }) => theme.palette.Grey.White}
+	${({ theme }) => theme.fontTheme.HEADLINE_03};
+	color: ${({ theme }) => theme.palette.Grey.White};
 `;
 
 const NumberTaskBox = styled.div`
@@ -88,3 +93,81 @@ const NumberText = styled.p`
 	${({ theme }) => theme.fontTheme.HEADLINE_02};
 	color: ${({ theme }) => theme.palette.Grey.White};
 `;
+
+const ImageArea = styled.div`
+	display: flex;
+`;
+const ImageComponent = styled.img`
+	width: 24.4rem;
+	height: 18.2rem;
+`;
+
+// const TaskSummaryLayout = styled.div`
+// 	display: flex;
+// 	flex-direction: row;
+// 	box-sizing: border-box;
+// 	width: 41.7rem;
+// 	height: 18.2rem;
+
+// 	background-color: ${({ theme }) => theme.palette.Blue.Blue5};
+// 	border-radius: 16px;
+// `;
+
+// const TextArea = styled.div`
+// 	display: flex;
+// 	flex-direction: column;
+// 	gap: 2rem;
+// 	margin-left: 2.8rem;
+// `;
+
+// const ProfileImg = styled.div`
+// 	width: 2.8rem;
+// 	height: 2.8rem;
+// 	margin: 3rem 34.5rem 0 0;
+
+// 	svg {
+// 		width: 100%;
+// 		height: 100%;
+// 	}
+// `;
+
+// const TextWrapper = styled.div`
+// 	display: flex;
+// 	flex-direction: column;
+// 	align-items: flex-start;
+// 	align-self: stretch;
+// 	width: 14.2rem;
+// 	height: 6.4rem;
+// 	margin-bottom: 3.5rem;
+// `;
+
+// const SummaryText = styled.p`
+// 	${({ theme }) => theme.fontTheme.HEADLINE_03}
+// 	color: ${({ theme }) => theme.palette.Grey.White}
+// `;
+
+// const NumberTaskBox = styled.div`
+// 	display: flex;
+// 	align-items: flex-end;
+// 	justify-content: center;
+// `;
+
+// const Number = styled.p`
+// 	${({ theme }) => theme.fontTheme.TITLE_02};
+// 	color: ${({ theme }) => theme.palette.Grey.White};
+// `;
+
+// const NumberText = styled.p`
+// 	margin-bottom: 0.4rem;
+
+// 	${({ theme }) => theme.fontTheme.HEADLINE_02};
+// 	color: ${({ theme }) => theme.palette.Grey.White};
+// `;
+
+// const ImageArea = styled.div`
+// 	display: flex;
+// `;
+// const ImageComponent = styled.img`
+// 	width: 24.4rem;
+// 	height: 18.2rem;
+// `;
