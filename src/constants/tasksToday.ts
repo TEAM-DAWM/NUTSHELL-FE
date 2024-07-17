0dd483aef48228a841f042593a73293ca15dfb62
JeeminYi <yjjmmm000@g.skku.edu>
2024-07-17 23:24:40 +0900

    chore: api 상수 데이터

--- src/constants/tasksToday.ts
+++ src/constants/tasksToday.ts
@@ -1,7 +1,24 @@
 const TODAY = {
 	code: 'success',
 	data: {
-		tasks: [],
+		tasks: [
+			{
+				id: 1,
+				name: 'task name',
+				deadLine: {
+					date: '2024-06-30',
+					time: '12:30',
+				},
+			},
+			{
+				id: 2,
+				name: 'task name',
+				deadLine: {
+					date: '2024-06-30',
+					time: '12:30',
+				},
+			},
+		],
 	},
 	message: null,
 };
