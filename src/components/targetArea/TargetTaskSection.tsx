d57909dfff19fd18a7d64870e6dbc240c94d0d2c
wrryu09 <rryu09@ewhain.net>
2024-07-17 22:56:34 +0900

    feat: targetArea api 연결

--- src/components/targetArea/TargetTaskSection.tsx
+++ src/components/targetArea/TargetTaskSection.tsx
@@ -3,6 +3,7 @@ import { Draggable } from 'react-beautiful-dnd';
 import BtnTask from '../common/BtnTask/BtnTask';
 import BtnTaskContainer from '../common/BtnTaskContainer';
 
+import useGetTasks from '@/apis/tasks/getTask/query';
 import { TaskType } from '@/types/tasks/taskType';
 
 interface TargetTaskSectionProps {
@@ -11,35 +12,41 @@ interface TargetTaskSectionProps {
 	tasks: TaskType[];
 }
 function TargetTaskSection(props: TargetTaskSectionProps) {
-	const { handleSelectedTarget, selectedTarget, tasks } = props;
-
+	const { handleSelectedTarget, selectedTarget } = props;
+	const targetDate = '2024-07-17';
+	const { isFetched, data } = useGetTasks({ targetDate });
+	console.log(data);
 	return (
 		<BtnTaskContainer type="target">
-			{tasks.map((task, index) => (
-				<Draggable key={task.id} draggableId={task.id.toString()} index={index}>
-					{(provided, snapshot) => (
-						<div
-							ref={provided.innerRef}
-							{...provided.draggableProps}
-							{...provided.dragHandleProps}
-							style={{ userSelect: 'none', ...provided.draggableProps.style }}
-						>
-							<BtnTask
-								iconType="active"
-								key={task.id}
-								hasDescription={task.hasDescription}
-								name={task.name}
-								deadLine={task.deadLine}
-								status={task.status}
-								id={task.id}
-								handleSelectedTarget={handleSelectedTarget}
-								selectedTarget={selectedTarget}
-								isDragging={snapshot.isDragging}
-							/>
-						</div>
-					)}
-				</Draggable>
-			))}
+			{isFetched && (
+				<>
+					{data.data.tasks.map((task: TaskType, index: number) => (
+						<Draggable key={task.id} draggableId={task.id.toString()} index={index}>
+							{(provided, snapshot) => (
+								<div
+									ref={provided.innerRef}
+									{...provided.draggableProps}
+									{...provided.dragHandleProps}
+									style={{ userSelect: 'none', ...provided.draggableProps.style }}
+								>
+									<BtnTask
+										iconType="active"
+										key={task.id}
+										hasDescription={task.hasDescription}
+										name={task.name}
+										deadLine={task.deadLine}
+										status={task.status}
+										id={task.id}
+										handleSelectedTarget={handleSelectedTarget}
+										selectedTarget={selectedTarget}
+										isDragging={snapshot.isDragging}
+									/>
+								</div>
+							)}
+						</Draggable>
+					))}
+				</>
+			)}
 		</BtnTaskContainer>
 	);
 }
