97d1d6dc5d768d23b01f21f9e58eecbc7bf7796b
moonseonghui <msh9980@naver.com>
2024-07-19 23:01:38 +0900

    feat: response code에 따라서 에러 토스트 구현

--- src/apis/timeBlocks/postTimeBlock/query.ts
+++ src/apis/timeBlocks/postTimeBlock/query.ts
@@ -3,15 +3,26 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import CreateTimeBlock from './axios';
 import { PostTimeBlokType } from './PostTimeBlockType';
 
+import { useToast } from '@/components/toast/ToastContext';
+
 const usePostTimeBlock = () => {
 	const queryClient = useQueryClient();
+	const { addToast } = useToast();
 
-	const mutation = useMutation({
-		mutationFn: ({ taskId, startTime, endTime }: PostTimeBlokType) => CreateTimeBlock({ taskId, startTime, endTime }),
+	const { mutate, isError } = useMutation({
+		mutationFn: async ({ taskId, startTime, endTime }: PostTimeBlokType) => {
+			const response = await CreateTimeBlock({ taskId, startTime, endTime });
+			/** response.code가 conflict일 때 타임블록 에러 토스트 출력 */
+			if (response && response.code === 'conflict') {
+				addToast(response.message);
+				throw new Error('conflict');
+			}
+			return response;
+		},
 		onSuccess: () => queryClient.invalidateQueries({ queryKey: ['timeblock'] }),
 	});
 
-	return { mutate: mutation.mutate };
+	return { mutate, isError };
 };
 
 export default usePostTimeBlock;
