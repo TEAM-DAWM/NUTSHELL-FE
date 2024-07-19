b136c31e1356817936044c517163e43677eaef53
moonseonghui <msh9980@naver.com>
2024-07-20 08:00:41 +0900

    feat: 타임블록 내에서 이동 토스트 띄우기

--- src/apis/timeBlocks/updateTimeBlock/query.ts
+++ src/apis/timeBlocks/updateTimeBlock/query.ts
@@ -3,12 +3,21 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import PatchTimeBlock from './axios';
 import { PatchTimeBlokType } from './PatchTimeBlockType';
 
+import { useToast } from '@/components/toast/ToastContext';
+
 const usePatchTimeBlock = () => {
 	const queryClient = useQueryClient();
+	const { addToast } = useToast();
 
 	const mutation = useMutation({
-		mutationFn: ({ taskId, timeBlockId, startTime, endTime }: PatchTimeBlokType) =>
-			PatchTimeBlock({ taskId, timeBlockId, startTime, endTime }),
+		mutationFn: async ({ taskId, timeBlockId, startTime, endTime }: PatchTimeBlokType) => {
+			const response = await PatchTimeBlock({ taskId, timeBlockId, startTime, endTime });
+			if (response && response.code === 'conflict') {
+				addToast(response.message);
+				throw new Error('conflict');
+			}
+			return response;
+		},
 		onSuccess: () => queryClient.invalidateQueries({ queryKey: ['timeblock'] }),
 	});
 
