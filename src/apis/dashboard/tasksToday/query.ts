f8323932b3f10ee98407029188e9a14fa49369ef
wrryu09 <rryu09@ewhain.net>
2024-07-20 05:14:56 +0900

    chore: placeholder data 빈배열

--- src/apis/dashboard/tasksToday/query.ts
+++ src/apis/dashboard/tasksToday/query.ts
@@ -6,24 +6,7 @@ import { TasksTodayType } from './TasksTodayType';
 const placeholderData = {
 	code: 'success',
 	data: {
-		tasks: [
-			{
-				id: 1,
-				name: 'task name',
-				deadLine: {
-					date: '2024-06-30',
-					time: '12:30',
-				},
-			},
-			{
-				id: 2,
-				name: 'task name',
-				deadLine: {
-					date: '2024-06-30',
-					time: '12:30',
-				},
-			},
-		],
+		tasks: [],
 	},
 	message: null,
 };
