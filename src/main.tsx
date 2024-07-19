fde15880415892b7bdbe6ae94535c5101dc4bdfc
moonseonghui <msh9980@naver.com>
2024-07-19 23:02:18 +0900

    feat: toast 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom/client';
 
 import App from '@/App.tsx';
+import { ToastProvider } from '@/components/toast/ToastContext';
 import GlobalStyle from '@/styles/GlobalStyle.tsx';
 import { theme } from '@/styles/theme.ts';
 
@@ -21,7 +22,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 	<QueryClientProvider client={queryClient}>
 		<GlobalStyle />
 		<ThemeProvider theme={theme}>
-			<App />
+			<ToastProvider>
+				<App />
+			</ToastProvider>
 		</ThemeProvider>
 		<ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" position="left" />
 	</QueryClientProvider>
